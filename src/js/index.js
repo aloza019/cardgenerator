f43f65ce8a9d51be6dbf000ea0db347c31ec7a24
aloza019 <andresafl@hotmail.com>
2020-09-29 00:08:46 +0000

    test

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,29 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let suit = generaterandomsuit();
+  if (suit == "&hearts;" || suit == "&diams;") {
+    document.querySelector("#topsuit").style.color = "red";
+    document.querySelector("#botoomsuit").style.color = "red";
+  }
+  document.querySelector("#topsuit").innerHTML = suit;
+  document.querySelector("#numbers").innerHTML = generaterandomnumber();
+  document.querySelector("#botoomsuit").innerHTML = suit;
+};
+
+let generaterandomsuit = () => {
+  let cardsuit = ["&hearts;", "&spades;", "&clubs;", "&diams;"];
+
+  let cardsuitrd = Math.floor(Math.random() * cardsuit.length);
+
+  return cardsuit[cardsuitrd];
+};
+
+let generaterandomnumber = () => {
+  let randomnum = ["A", "2", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let randomnumberrd = Math.floor(Math.random() * randomnum.length);
+
+  return randomnum[randomnumberrd];
 };
